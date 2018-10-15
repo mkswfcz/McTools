51730e287eb01cbc92e3f02464a7cb777cffcb0c
yuntaocao <2693925861@qq.com>
2018-10-15 16:25:16 +0800

    test

---
+++ test/test_base.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apple
+ * Date: 2018/10/15
+ * Time: 下午4:24
+ */
+$path = realpath(__DIR__.'/../public/index.php');
+require "{$path}";
--- test/test_common.php
+++ test/test_common.php
@@ -5,7 +5,7 @@
  * Date: 2018/10/15
  * Time: 上午10:22
  */
-$path = realpath(__DIR__.'/../public/index.php');
-require "{$path}";
-debug('time: ',myDate(),myDate('2018-10-15 10:54:57','digital'));
+require "test_base.php";
+
+debug('time: ', myDate(), myDate('2018-10-15 10:54:57', 'digital'));
 debug(Articles::findFirstById(1));
