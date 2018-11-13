042c74ee6c4d521ff11c7e575509995f7ebcd3dd
apple <apple@appledeiMac-10.local>
2018-11-13 21:14:29 +0800

    类比区块浏览器的相关接口

--- app/tasks/EosTask.php
+++ app/tasks/EosTask.php
@@ -15,12 +15,16 @@ class EosTask extends Phalcon\Cli\Task
         echo $api->getBlock("1337") . PHP_EOL;
         echo $api->getBlockHeaderState("0016e48707b181d93117b07451d9837526eba34a9a37125689fb5a73a5d28a38") . PHP_EOL;
         echo $api->getAccount("blockmatrix1") . PHP_EOL;
-        echo $api->getCode("eosio.token") . PHP_EOL;
+        echo $api->getCode("eosio.token") . PHP_EOL; //此方法废弃
         echo $api->getTableRows("eosio", "eosio", "producers", ["limit" => 10]) . PHP_EOL;
-        echo $api->getCurrencyBalance("eosio.token", "eosdacserver") . PHP_EOL;
+        echo $api->getCurrencyBalance("eosio.token", "atticlabeosb") . PHP_EOL;
         echo $api->getCurrencyStats("eosio.token", "EOS") . PHP_EOL;
-        echo $api->getAbi("eosio.token") . PHP_EOL;
+
+        #支持的actions...
+        echo $api->getAbi("eosio.token") . PHP_EOL; //等价于getCode
         echo $api->getProducers(10) . PHP_EOL;
+
+        #二进制和json数据转化
         echo $api->abiJsonToBin("eosio.token", "transfer", ["blockmatrix1", "blockmatrix1", "7.0000 EOS", "Testy McTest"]) . PHP_EOL;
         echo $api->abiBinToJson("eosio.token", "transfer", "10babbd94888683c10babbd94888683c701101000000000004454f53000000000c5465737479204d6354657374") . PHP_EOL;
     }
