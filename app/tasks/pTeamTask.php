9a747ac33e54a8f8d2d9bcad467932d7b05126f6
yuntaocao <2693925861@qq.com>
2018-12-07 21:33:42 +0800

    test async task

--- app/tasks/pTeamTask.php
+++ app/tasks/pTeamTask.php
@@ -36,6 +36,9 @@ class pTeamTask extends \Phalcon\Cli\Task
 
     function isParent($pid)
     {
+        if ($pid > 0) {
+            debug('pid: ',$pid);
+        }
         return $pid > 0;
     }
 
@@ -47,6 +50,13 @@ class pTeamTask extends \Phalcon\Cli\Task
     function getPid()
     {
         return posix_getpid();
+    }
+
+    function getRedis()
+    {
+        $cache = getConfig('cache');
+        $end_point = $cache->redis->endpoint;
+        return McRedis::getInstance($end_point);
     }
 
     function isMainProcessExit()
@@ -71,7 +81,7 @@ class pTeamTask extends \Phalcon\Cli\Task
                 echoTip("Monitor 进程 退出!");
                 break;
             } else {
-                echoTip("select and clear timeout process! ");
+//                echoTip("select and clear timeout process! ");
                 usleep(1000);
             }
         }
@@ -81,7 +91,7 @@ class pTeamTask extends \Phalcon\Cli\Task
 
     static function hello($someone)
     {
-        echo "hello world! $someone" . PHP_EOL;
+//        echo "hello world! $someone" . PHP_EOL;
     }
 
     function popTask()
@@ -99,36 +109,98 @@ class pTeamTask extends \Phalcon\Cli\Task
         call_user_func($clazz . '::' . $method, ...$params);
     }
 
+    function getWorkProcessKey()
+    {
+        return 'work_process_cache_key_' . $this->queue;
+    }
+
     function createWorker()
     {
         $pid = pcntl_fork();
         if ($this->isForkFail($pid)) {
             die("Worker fork error!");
         } elseif ($this->isParent($pid)) {
-            $pid = $this->getPid();
             echoTip("Create Worker! pid: {$pid}");
-            return;
+            return $pid;
         }
 
         while (true) {
             if ($this->isMainProcessExit()) {
-                echoTip("Monitor 进程 退出!");
+                echoTip("Monitor 进程 退出 子进程退出循环!");
                 break;
             } else {
                 $this->executeTask();
             }
         }
+        exit(0);
     }
+
+    function isNeedCreateWorker()
+    {
+        $value_counts = array_count_values(self::$child_processes);
+        if (isset($value_counts[$this->queue])) {
+            if ($value_counts[$this->queue] < $this->process_num) {
+                return true;
+            }
+            return false;
+        }
+        sleep(5);
+        debug('current: ', self::$child_processes);
+        return true;
+    }
+
+    function recoverChildProcess()
+    {
+        $pid = pcntl_wait($status, WUNTRACED | WNOHANG);
+        if ($pid) {
+            if (isset(self::$child_processes[$pid])) {
+                debug("Worker Pid {$pid} exit!");
+                unset(self::$child_processes[$pid]);
+            }
+        }
+    }
+
 
     function startAction()
     {
-        $pid = $this->getPid();
-        echoTip("main pid: {$pid}");
-        $this->setQueueName();
+        $pid = pcntl_fork();
 
-        $this->monitor();
+        if ($this->isForkFail($pid)) {
+            die("Monitor Fork fail!");
+        } elseif ($pid > 0) {
+            debug("return pid: ", posix_getpid());
+            return $pid;
+        } else {
 
+            echoTip("Monitor pid: {$pid}");
+            $this->setQueues();
 
-        echoTip("main exit!");
+            $this->monitor();
+
+            while (true) {
+
+                if ($this->isNeedCreateWorker()) {
+
+                    $worker_pid = $this->createWorker();
+                    if ($worker_pid) {
+                        debug("Worker Pid {$worker_pid} create!");
+                        self::$child_processes[$worker_pid] = $this->queue;
+                    }
+
+                }
+//                debug("Wait Recover ChildProcess!");
+                $this->recoverChildProcess();
+                usleep(1000);
+            }
+            echoTip("main exit!");
+        }
+    }
+
+    function testAction()
+    {
+        $redis = $this->getRedis();
+        $value = $redis->set('test_a', 1);
+        $value = $redis->get('test_a');
+        debug($value);
     }
 }
